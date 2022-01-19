3a0800a805602501e1a0fd157c3505f2e86af9b6
wNiedzielski <wojciech.niedzielski@dmt.com.pl>
2022-01-19 14:05:37 +0100

    poprawki

--- logger/logger.cpp
+++ logger/logger.cpp
@@ -6,10 +6,7 @@ LOG::Logger *LOG::Logger::singleLog = nullptr;
 LOG::Logger::Logger()
 {
     log_file.open(log_file_dest, std::fstream::trunc);
-    if (log_file.is_open())
-        log_file << "Hej!" << std::endl;
-    else
-        std::cout << "Error" << std::endl;
+    addTrace("Start log");
 }
 
 LOG::Logger *LOG::Logger::getInstance()
@@ -21,6 +18,7 @@ LOG::Logger *LOG::Logger::getInstance()
 
 LOG::Logger::~Logger()
 {
+    addTrace("End log!");
     log_file.close();
 }
 
@@ -28,5 +26,8 @@ void LOG::Logger::addTrace(std::string trace)
 {
     // add lock on mutex to prevent too much traceing add
     std::lock_guard<std::mutex> guard(mutex_controler);
-    log_file << trace << std::endl;
+    if (log_file.is_open())
+        log_file << trace << std::endl;
+    else
+        std::cerr << "Log file is not open!";
 }
